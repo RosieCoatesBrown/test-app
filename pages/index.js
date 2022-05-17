4703462bd8305ef08b4d6d4e4c34b53204170b87
Rosie Coates-Brown <rosanna.coates-brown@coopdigital.co.uk>
2022-05-17 11:20:29 +0100

    add server side rendered props

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,8 +13,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Co-op!</a>
         </h1>
+        <h2>{data}</h2>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -66,4 +67,13 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const data = `hello the time is ${Date.now()}`
+  return {
+    props: {
+      data
+    },
+  }
 }
